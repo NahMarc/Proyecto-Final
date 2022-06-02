be12a7364aeee4fec75d91129edad806c2e7afe8
Nahuel <ncarrasanperez@gmail.com>
2022-06-01 22:42:49 -0300

    se realizo la maqueta con los componentes de header, banner, about, exp, educ y habilidades.

--- ProyectoFinalNahuel/src/app/app.module.ts
+++ ProyectoFinalNahuel/src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { NgModule } from '@angular/core';
+import { HardsoftComponent } from './components/hardsoft/hardsoft.component';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LogoArgProgComponent } from './components/logo-arg-prog/logo-arg-prog.component';
+import { RedesSocComponent } from './components/redes-soc/redes-soc.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LogoArgProgComponent,
+    RedesSocComponent,
+    BannerComponent,
+    SobreMiComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HardsoftComponent,
+    ProyectosComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({}),
   ],
   providers: [],
   bootstrap: [AppComponent]
