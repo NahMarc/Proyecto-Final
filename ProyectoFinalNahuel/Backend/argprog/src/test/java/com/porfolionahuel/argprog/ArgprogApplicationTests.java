573ed1ed951bf77bdcd8d0b5b96334f5718cab28
Nahuel <ncarrasanperez@gmail.com>
2022-06-25 01:39:30 -0300

    Se crea la clase Persona y la Interfaz Persona

---
+++ ProyectoFinalNahuel/Backend/argprog/src/test/java/com/porfolionahuel/argprog/ArgprogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porfolionahuel.argprog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgprogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
