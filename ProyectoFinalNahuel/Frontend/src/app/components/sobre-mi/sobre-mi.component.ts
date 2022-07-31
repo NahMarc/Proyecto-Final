183af2e259371f68b4b576674d29cae83cc12e6f
Nahuel <ncarrasanperez@gmail.com>
2022-07-31 14:27:33 -0300

    Se agregaron links de contacto y se decidió dejar sin edicion al componente sobre mí

--- ProyectoFinalNahuel/Frontend/src/app/components/sobre-mi/sobre-mi.component.ts
+++ ProyectoFinalNahuel/Frontend/src/app/components/sobre-mi/sobre-mi.component.ts
@@ -1,6 +1,4 @@
-import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Route, Router } from '@angular/router';
 import { persona } from 'src/app/model/persona.model';
 import { PersonaService } from 'src/app/service/persona.service';
 import { TokenService } from 'src/app/service/token.service';
@@ -11,59 +9,28 @@ import { TokenService } from 'src/app/service/token.service';
   styleUrls: ['./sobre-mi.component.css']
 })
 export class SobreMiComponent implements OnInit {
-  persona: persona = new persona("", "", "", "", "", "");
-  editPersona: persona = null;
+  perso: persona = new persona("", "", "", "", "", "");
   
 
-  constructor(public personaService: PersonaService, private tokenService: TokenService, private activatedRoute: ActivatedRoute, private router: Router) { }
+  constructor(public personaService: PersonaService, private tokenService: TokenService) { }
 
   isLogged = false;
 
   ngOnInit(): void {
-    this.personaService.getPersona().subscribe(data => {this.persona = data})
+    this.cargarPersona();
+
     if(this.tokenService.getToken()){
       this.isLogged = true;
-    }else {
+    } else {
       this.isLogged = false;
     }
   }
 
-  public getPersona(): void {
-    this.personaService.getPersona().subscribe({
-      next: (Response: persona) => {
-        this.persona=Response;
-      },
-      error: (error: HttpErrorResponse) => {
-        alert(error.message);
-      }
-    })
-  }
-
-
-  public onEditPersona(perso: persona): void {
-    this.personaService.editPersona(perso).subscribe({
-      next: (Response: persona) => {
-        console.log(Response);
-        this.getPersona();
-      },
-      error: (error: HttpErrorResponse) =>{
-        alert(error.message);
-      }
-    })
-  }
-
-  public onOpenModal(mode: string, persona?: persona): void {
-    const container = document.getElementById('usermodal');
-    const button = document.createElement('button');
-    button.type = 'button';
-    button.style.display = 'none';
-    button.setAttribute('data-toggle', 'modal');    
-    if (mode === 'edit') {
-      this.editPersona = persona;
-      button.setAttribute('data-target', '#editUserModal');
-    }    
-    container?.appendChild(button);
-    button.click();
+  cargarPersona(): void{
+    this.personaService.getPersona().subscribe(
+      data => {this.perso = data;}
+    )
+  
   }
 
 }
--- ProyectoFinalNahuel/Frontend/src/app/service/persona.service.ts
+++ ProyectoFinalNahuel/Frontend/src/app/service/persona.service.ts
@@ -8,17 +8,13 @@ import { persona } from '../model/persona.model';
 })
 
 export class PersonaService {
-  URL = 'http://localhost:8080/personas';
-/* http://localhost:8080 */
+  URL = 'http://localhost:8080/personas/';
 
   constructor(private httpClient: HttpClient) { }
 
   public getPersona(): Observable<persona>{
-    return this.httpClient.get<persona>(this.URL+ '/traer/perfil');
+    return this.httpClient.get<persona>(this.URL+ 'traer/perfil');
   }
 
-  public editPersona(perso: persona):Observable<any>{
-    return this.httpClient.put<any>(`${this.URL}/edit`, perso);
-    // return this.httpClient.put<any>(this.URL+ `edit/${id}`, perso);
-  }
+
 }
